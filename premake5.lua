f9928b615a53e5b30caffd56394d983be6530e00
git-n8 <94858810+git-n8@users.noreply.github.com>
2022-05-17 21:31:24 -0300

    Adding premake and kafka

---
+++ premake5.lua
@@ -0,0 +1,47 @@
+workspace "In.spire"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+    outputdir = "%{cfg.buildcfg}_%{cfg.system}_%{cfg.architecture}"
+
+    project "In.spire"
+        location "In.spire"
+        kind "ConsoleApp"
+        language "C++"
+
+        targetdir ("_bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("_obj/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp"
+        }
+
+        includedirs
+        {
+            "%{prj.name}/thirdparty/spdlog/include"
+        }
+
+        filter "system:windows"
+            cppdialect "C++17"
+            staticruntime "On"
+            systemversion "latest"
+
+        filter "configurations:Debug"
+            defines "IS_DEBUG"
+            symbols "On"
+
+        filter "configurations:Release"
+            defines "IS_RELEASE"
+            optimize "On"
+
+        filter "configurations:Dist"
+            defines "IS_DIST"
+            optimize "On"
